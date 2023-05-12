cffbb05166155d6bb0ae017b3a0378fc8281569c
yamamuke <yamamukeisuke@gmail.com>
2023-05-12 15:55:16 +0900

    test storage link

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('storage/app/public'),
+        public_path('storage') => storage_path('app/public'),
         // public_path('storage/images') => storage_path('app/public/images'),
     ],
 
